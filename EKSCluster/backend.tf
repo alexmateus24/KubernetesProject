789d31408c43f1caade2216c4dce1fcf399e6a52
alexmateus24 <alexmateus247@live.com>
2026-02-10 13:14:35 +0000

    Update backend.tf

--- EKSCluster/backend.tf
+++ EKSCluster/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "XX" # CHANGE WITH YOUR S3 BUCKET
+    bucket = "myfirsts3bucketalex"
     key    = "K8inEKS/terraform.tfstate"
     region = "eu-west-2"
   }
